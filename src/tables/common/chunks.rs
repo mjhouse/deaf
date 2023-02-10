db013092ddfec2c7c6a577face579dcc86086e3c
Michael House <mjhouse@protonmail.com>
2023-02-10 07:46:25 -0600

    Refactored the byte iterator and added tests

--- src/tables/common/chunks.rs
+++ src/tables/common/chunks.rs
@@ -24,55 +24,45 @@ impl<'a> ByteIterator<'a> {
         }
     }
 
-    // pub fn new(data: &'a [u8]) -> Self {
-    //     Self { 
-    //         data: data,
-    //         delim: ByteDelimiter::Length(0),
-    //         limit: data.len(),
-    //         index: 0,
-    //         max: data.len(),
-    //         finished: false,
-    //     }
-    // }
+    fn take_slice(&self) -> Option<&'a [u8]> {
+        if self.index <= self.limit {
+            Some(&self.data[self.index..])
+        }
+        else {
+            None
+        }
+    }
 
-    // pub fn until_value<T: Into<u8>>(mut self, value: T) -> Self {
-    //     self.delim = ByteDelimiter::Value(value.into());
-    //     self
-    // }
+    fn find_value(&mut self, value: u8) -> Option<usize> {
+        self.take_slice()
+            .and_then(|d| d
+                .iter()
+                .position(|&i| i == value)
+                .map(|i| i + 1))
+    }
 
-    // pub fn until_length(mut self, length: usize) -> Self {
-    //     self.delim = ByteDelimiter::Length(length);
-    //     self
-    // }
+    fn find_length(&mut self, value: usize) -> Option<usize> {
+        self.take_slice()
+            .and_then(|d| if d.len() >= value {
+                Some(value)
+            } else {
+                None
+            })
+    }
 
-    // pub fn with_limit(mut self, limit: usize) -> Self {
-    //     self.limit = limit;
-    //     self
-    // }
+    fn take_value(&mut self, value: u8) -> Option<&'a [u8]> {
+        self.find_value(value)
+            .and_then(|i| self
+                .take_slice()
+                .map(|d| &d[..i]))
+    }
 
-    // pub fn finish(mut self) -> Self {
-    //     let mut finished = true;
-
-    //     if self.limit == 0 {
-    //         println!("ByteIterator used with a limit of 0");
-    //         finished = false;
-    //     }
-
-    //     if self.max == 0 {
-    //         println!("ByteIterator given 0-length byte slice");
-    //         finished = false;
-    //     }
-
-    //     if let ByteDelimiter::Length(v) = self.delim {
-    //         if v == 0 {
-    //             println!("ByteIterator given entity length of 0");
-    //             finished = false;
-    //         }
-    //     }
-
-    //     self.finished = finished;
-    //     self
-    // }
+    fn take_length(&mut self, length: usize) -> Option<&'a [u8]> {
+        self.find_length(length)
+            .and_then(|i| self
+                .take_slice()
+                .map(|d| &d[..i]))
+    }
 
 }
 
@@ -80,28 +70,13 @@ impl<'a> Iterator for ByteIterator<'a> {
     type Item = &'a [u8];
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut result = None;
+        let result = match self.delim {
+            ByteDelimiter::Value(v) => self.take_value(v),  // self.index += end + 1;
+            ByteDelimiter::Length(v) => self.take_length(v),// self.index += length;
+        };
 
-        // early return if we're finished
-        if self.index > self.limit {
-            return result;
-        }
-
-        let data = &self.data[self.index..];
-
-        match self.delim {
-            ByteDelimiter::Value(v) => {
-                if let Some(n) = data.iter().position(|&k| k == v) {
-                    result = Some(&data[..n]);
-                    self.index += n + 1;
-                }
-            },
-            ByteDelimiter::Length(v) => {
-                if data.len() >= v {
-                    result = Some(&data[..v]);
-                    self.index += v;
-                }
-            }
+        if let Some(d) = result {
+            self.index += d.len();
         }
 
         result
@@ -124,21 +99,84 @@ mod tests {
     ];
 
     #[test]
-    fn test_iterate_by_null_terminators() {
+    fn test_byte_iter_take_slice() {
         let mut iter = ByteIterator::new(NULL_TERM_STR,ByteDelimiter::Value(b'\0'));
-        let mut count = 0;
-        for chunk in iter {
-            dbg!(std::str::from_utf8(chunk));
-        }
+        assert!(iter.take_slice().is_some());
+
+        // returns whole slcie because the iter index defaults to 0
+        let result = iter.take_slice().unwrap();
+        assert_eq!(result,NULL_TERM_STR);
     }
 
     #[test]
-    fn test_iterate_by_fixed_length() {
-        let mut iter = ByteIterator::new(FIXED_LEN_DATA,ByteDelimiter::Length(14));
-        let mut count = 0;
-
-        for chunk in iter {
-            dbg!(std::str::from_utf8(chunk));
-        }
+    fn test_byte_iter_find_value() {
+        let mut iter = ByteIterator::new(NULL_TERM_STR,ByteDelimiter::Value(b'\0'));
+        let result = iter.find_value(b'\0');
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(),15);
     }
+
+    #[test]
+    fn test_byte_iter_find_length() {
+        let mut iter = ByteIterator::new(NULL_TERM_STR,ByteDelimiter::Length(14));
+        let result = iter.find_length(14);
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(),14);
+    }
+
+    #[test]
+    fn test_byte_iter_take_value() {
+        let mut iter = ByteIterator::new(NULL_TERM_STR,ByteDelimiter::Value(b'\0'));
+        let result = iter.take_value(b'\0');
+
+        assert!(result.is_some());
+        
+        let data = result.unwrap();
+        let string = std::str::from_utf8(data).unwrap();
+        
+        assert_eq!(data.len(),15);
+        assert_eq!(string,"This is a test\0");
+    }
+
+    #[test]
+    fn test_byte_iter_take_length() {
+        let mut iter = ByteIterator::new(NULL_TERM_STR,ByteDelimiter::Length(14));
+        let result = iter.take_length(14);
+
+        assert!(result.is_some());
+        
+        let data = result.unwrap();
+        let string = std::str::from_utf8(data).unwrap();
+        
+        assert_eq!(data.len(),14);
+        assert_eq!(string,"This is a test");
+    }
+
+    #[test]
+    fn test_byte_iter_by_value() {
+        let mut iter = ByteIterator::new(NULL_TERM_STR,ByteDelimiter::Value(b'\0'));
+        let strings = iter
+            .filter_map(|d| std::str::from_utf8(d).ok())
+            .map(|s| s.into())
+            .collect::<Vec<String>>();
+
+
+        assert_eq!(strings.len(),2);
+        assert_eq!(strings[0],"This is a test\0");
+        assert_eq!(strings[1],"Another test\0");
+    }
+
+    #[test]
+    fn test_byte_iter_by_length() {
+        let mut iter = ByteIterator::new(FIXED_LEN_DATA,ByteDelimiter::Length(14));
+        let strings = iter
+            .filter_map(|d| std::str::from_utf8(d).ok())
+            .map(|s| s.into())
+            .collect::<Vec<String>>();
+
+        assert_eq!(strings.len(),2);
+        assert_eq!(strings[0],"This is a test");
+        assert_eq!(strings[1],"This is a test");
+    }
+
 }
