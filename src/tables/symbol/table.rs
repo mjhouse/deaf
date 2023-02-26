039b1b147c24cd635db36bc3bb849c4a94c6ed62
Michael House <mjhouse@protonmail.com>
2023-02-25 18:23:56 -0600

    Manually verifying that pipeline fails when tests fail

--- src/tables/symbol/table.rs
+++ src/tables/symbol/table.rs
@@ -170,7 +170,8 @@ mod tests {
     const TEST_TABLE_LENGTH: usize = 7056;
 
     // the number of elements in the test table
-    const TEST_TABLE_COUNT: usize = 294;
+    const TEST_TABLE_COUNT: usize = 100; //294;
+    // TEST: MAKING TESTS FAIL FOR CI/CD PIPELINE
 
     // the size of an element in the test table
     const TEST_TABLE_ENTITY: usize = 24;
