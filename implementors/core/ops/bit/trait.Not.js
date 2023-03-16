9bcbeb28264127eb981c5d5a9eac1f92715e863a
mjhouse <mjhouse@users.noreply.github.com>
2023-03-16 15:47:21 +0000

    deploy: 3b8ad2e6bec2d9eb4fdad9326458a39f2346f2e1

--- implementors/core/ops/bit/trait.Not.js
+++ implementors/core/ops/bit/trait.Not.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"deaf":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"deaf/common/enum.SHFlags.html\" title=\"enum deaf::common::SHFlags\">SHFlags</a>"]]
+"deaf":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"deaf/common/enum.SHFlags.html\" title=\"enum deaf::common::SHFlags\">SHFlags</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
