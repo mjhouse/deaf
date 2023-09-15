f690a01d9e3ad8711561cff9b9333bc3e2500196
mjhouse <mjhouse@users.noreply.github.com>
2023-09-15 12:45:17 +0000

    deploy: 8b6a8ebf64d3529c765624b417b6331e2e8c7412

--- deaf/common/sidebar-items.js
+++ deaf/common/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ByteDelimiter","Layout","PHType","SHFlags","SHType","STBind","STType","SectionType","Width"],"mod":["ranges"],"struct":["All","ByteIter","Field","Item","Update"],"trait":["Convert","FromBytes","IntoBytes","Updateable"]};
+window.SIDEBAR_ITEMS = {"enum":["ByteDelimiter","Layout","PHType","SHFlags","SHType","STBind","STType","SectionType","Width"],"mod":["ranges"],"struct":["All","ByteIter","Field","Item","ItemArray","Update"],"trait":["Convert","FromBytes","IntoBytes","T32Value","T64Value","TOutValue","Updateable"]};
