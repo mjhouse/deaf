b4cc7216cb33522fd8997c92d3844a128f9aacd7
mjhouse <mjhouse@users.noreply.github.com>
2023-10-09 12:39:41 +0000

    deploy: d35fd251d9da1e5d168e6717a45693148d057260

--- implementors/num_enum/trait.FromPrimitive.js
+++ implementors/num_enum/trait.FromPrimitive.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"deaf":[["impl FromPrimitive for <a class=\"enum\" href=\"deaf/common/enum.SHType.html\" title=\"enum deaf::common::SHType\">SHType</a>"]]
+"deaf":[["impl FromPrimitive for <a class=\"enum\" href=\"deaf/common/enum.STBind.html\" title=\"enum deaf::common::STBind\">STBind</a>"],["impl FromPrimitive for <a class=\"enum\" href=\"deaf/common/enum.STVisibility.html\" title=\"enum deaf::common::STVisibility\">STVisibility</a>"],["impl FromPrimitive for <a class=\"enum\" href=\"deaf/common/enum.SHType.html\" title=\"enum deaf::common::SHType\">SHType</a>"],["impl FromPrimitive for <a class=\"enum\" href=\"deaf/common/enum.STType.html\" title=\"enum deaf::common::STType\">STType</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"deaf":["",[["common",[],["bytes.rs","constants.rs","enums.rs","field.rs","item.rs","item_array.rs","iterator.rs","mod.rs","ranges.rs","update.rs"]],["headers",[],["file.rs","mod.rs","program.rs","section.rs"]],["tables",[["info",[],["mod.rs","relocation.rs","symbol.rs"]],["items",[],["array.rs","mod.rs","relocation.rs","string.rs","symbol.rs","traits.rs"]]],["mod.rs","table.rs"]]],["binary.rs","errors.rs","lib.rs","section.rs","segment.rs","utilities.rs"]]\
+"deaf":["",[["common",[],["bytes.rs","constants.rs","enums.rs","field.rs","item.rs","item_array.rs","iterator.rs","mod.rs","ranges.rs","update.rs"]],["functions",[],["function.rs","mod.rs"]],["headers",[],["file.rs","mod.rs","program.rs","section.rs"]],["symbols",[],["info.rs","mod.rs","symbol.rs"]],["tables",[["info",[],["mod.rs","relocation.rs"]],["items",[],["array.rs","mod.rs","relocation.rs","string.rs","traits.rs"]]],["mod.rs","table.rs"]]],["binary.rs","errors.rs","lib.rs","section.rs","segment.rs","utilities.rs"]]\
 }');
 createSourceSidebar();
