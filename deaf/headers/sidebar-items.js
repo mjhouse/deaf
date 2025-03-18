5222d99043387362e63e57e4a54df9a633c7919a
mjhouse <mjhouse@users.noreply.github.com>
2025-03-18 14:56:30 +0000

    deploy: 355db1096e8eb3a8957427e8bda581bd630e2660

--- deaf/headers/sidebar-items.js
+++ deaf/headers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["FileHeader","ProgramHeader","SectionHeader","SectionHeaderData"]};
+window.SIDEBAR_ITEMS = {"struct":["FileHeader","ProgramHeader","SectionHeader"]};
